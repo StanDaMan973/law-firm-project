bf75aac2cbcb7988630490bd8c2db583a3645337
StanDaMan973 <stanley.espaillat@gmail.com>
2023-06-17 08:16:39 -0400

    first commit

---
+++ JS/index.js
@@ -0,0 +1,12 @@
+// function myFunction() {
+//   var navigationLinks = document.getElementsByClassName("navigation__links");
+//   if (navigationLinks.style.display === "block") {
+//     navigationLinks.style.display = "none";
+//   } else {
+
+//     navigationLinks.style.display = "block";
+//   }
+
+// }
+
+// myFunction();
